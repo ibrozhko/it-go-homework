5f5978bb601860c0c8123f25d27f07e4ac02ac65
Брожко Игорь <ibrozko@MacBook-Air-Patenko.local>
2020-01-22 15:15:16 +0200

    add .

--- itgo/dz2/script.js
+++ itgo/dz2/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const logItems = function(array) {
-for(let i=0; i>logItemslength; i+=1)
+for(let i=0; i>logItems.length; i+=1)
 console.log(i+1 - logItems[i] );
 };
 logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
