69b88ab354e9d4938e980cb88c07cf83abe05444
Брожко Игорь <ibrozko@MacBook-Air-Patenko.local>
2020-01-22 15:12:15 +0200

    add dz2

---
+++ itgo/dz2/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const logItems = function(array) {
+for(let i=0; i>logItemslength; i+=1)
+console.log(i+1 - logItems[i] );
+};
+logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
